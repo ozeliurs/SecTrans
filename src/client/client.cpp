b9bb79cb3598dfa0036a125254ae9c61ec22fee9
Ozeliurs <ozeliurs@gmail.com>
2023-12-23 19:39:55 +0100

    feat: add basic messaging on client

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -7,23 +7,70 @@
 #include <getopt.h>
 #include <string>
 #include <cstring>
+#include "../lib/client.h"
 
-void uploadCommand(const char* file) {
-    std::cout << "Executing upload command with file: " << file << "\n";
-    // Add your upload logic here
-}
+enum Action {
+    UPLOAD,
+    DOWNLOAD,
+    LIST
+};
 
-void downloadCommand(const char* file) {
-    std::cout << "Executing download command with file: " << file << "\n";
-    // Add your download logic here
-}
 
-void listCommand() {
-    std::cout << "Executing list command\n";
-    // Add your list logic here
-}
+class Message {
+private:
+    Action action;
+    std::string filename;
+    int MESSAGE_SIZE = 1024;
+public:
+    Message(Action action, std::string filename) {
+        this->action = action;
+        this->filename = filename;
+    }
 
-void helpCommand(const std::string& command) {
+    char *toString() {
+        std::string str;
+
+        switch (action) {
+            case UPLOAD:
+                str[0] = 'U';
+                break;
+            case DOWNLOAD:
+                str[0] = 'D';
+                break;
+            case LIST:
+                str[0] = 'L';
+                break;
+        }
+
+        if (filename.length() > MESSAGE_SIZE - 1) {
+            std::cerr << "File name is too long (max " << MESSAGE_SIZE - 1 << " characters)" << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+
+        str = str + filename;
+
+        return (char *) str.c_str();
+    }
+};
+
+class Client {
+public:
+    void upload(const char *file) {
+        std::string filename = file;
+        sndmsg(Message(UPLOAD, filename).toString(), 8080);
+    }
+
+    void download(const char *file) {
+        std::string filename = file;
+        sndmsg(Message(DOWNLOAD, filename).toString(), 8080);
+    }
+
+    void list() {
+        sndmsg(Message(LIST, "").toString(), 8080);
+    }
+};
+
+void helpCommand(const std::string &command) {
     std::cout << "Executing help command\n";
     std::cerr << "Usage: " << std::endl;
     std::cerr << "\t" << command << " --upload <file>" << std::endl;
@@ -40,23 +87,24 @@ int main(int argc, char *argv[]) {
     static struct option long_options[] = {
             {"upload",   required_argument, nullptr, 'u'},
             {"download", required_argument, nullptr, 'd'},
-            {"list",     no_argument,       nullptr, 'l'},
-            {nullptr,    0,                 nullptr, 0}
+            {"list",     no_argument,       nullptr, 'l'}
     };
+
+    Client client = Client();
 
     int option;
     while ((option = getopt_long(argc, argv, "ud:l", long_options, nullptr)) != -1) {
         switch (option) {
             case 'u':
-                uploadCommand(optarg);
+                client.upload(optarg);
                 break;
 
             case 'd':
-                downloadCommand(optarg);
+                client.download(optarg);
                 break;
 
             case 'l':
-                listCommand();
+                client.list();
                 break;
 
             default:
