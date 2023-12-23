2b62283c19ba090c5dd85cce6d4eec2da2b4d26f
Ozeliurs <ozeliurs@gmail.com>
2023-12-23 18:05:44 -0500

    feat: try to resolve cmake problem

--- src/server/SecTransServer.cpp
+++ src/server/SecTransServer.cpp
@@ -13,27 +13,43 @@
 #include "../lib/ultimateServer.h"
 
 /*
-[main] Building folder: SecTrans SecTransServer
+[main] Building folder: SecTrans SecTransClient
 [build] Starting build
-[proc] Executing command: /usr/bin/cmake --build /home/ozeliurs/SecTrans/build --config Debug --target SecTransServer -j 4 --
-[build] Scanning dependencies of target SecTransServer
-[build] [ 33%] Building CXX object CMakeFiles/SecTransServer.dir/src/server/SecTransServer.cpp.o
-[build] [ 66%] Linking CXX executable SecTransServer
-[build] /usr/bin/ld: CMakeFiles/SecTransServer.dir/src/server/SecTransServer.cpp.o: in function `send(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, int)':
-[build] /home/ozeliurs/SecTrans/src/server/../lib/ultimateClient.h:28: undefined reference to `sndmsg(char*, int)'
-[build] /usr/bin/ld: CMakeFiles/SecTransServer.dir/src/server/SecTransServer.cpp.o: in function `start(int)':
-[build] /home/ozeliurs/SecTrans/src/server/../lib/ultimateServer.h:10: undefined reference to `startserver(int)'
-[build] /usr/bin/ld: CMakeFiles/SecTransServer.dir/src/server/SecTransServer.cpp.o: in function `stop()':
-[build] /home/ozeliurs/SecTrans/src/server/../lib/ultimateServer.h:15: undefined reference to `stopserver()'
-[build] /usr/bin/ld: CMakeFiles/SecTransServer.dir/src/server/SecTransServer.cpp.o: in function `receive(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&)':
-[build] /home/ozeliurs/SecTrans/src/server/../lib/ultimateServer.h:26: undefined reference to `getmsg(char*)'
+[proc] Executing command: /usr/bin/cmake --build /home/ozeliurs/SecTrans/build --config Debug --target SecTransClient -j 4 --
+[build] [ 66%] Building CXX object CMakeFiles/SecTransClient.dir/src/client/client.cpp.o
+[build] [ 66%] Building CXX object CMakeFiles/SecTransClient.dir/src/client/SecTransClient.cpp.o
+[build] [100%] Linking CXX executable SecTransClient
+[build] /usr/bin/ld: CMakeFiles/SecTransClient.dir/src/client/SecTransClient.cpp.o: in function `send(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, int)':
+[build] /home/ozeliurs/SecTrans/src/client/../lib/ultimateClient.h:8: multiple definition of `send(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, int)'; CMakeFiles/SecTransClient.dir/src/client/client.cpp.o:/home/ozeliurs/SecTrans/src/client/../lib/ultimateClient.h:8: first defined here
+[build] /usr/bin/ld: CMakeFiles/SecTransClient.dir/src/client/SecTransClient.cpp.o: in function `start(int)':
+[build] /home/ozeliurs/SecTrans/src/client/../lib/ultimateServer.h:8: multiple definition of `start(int)'; CMakeFiles/SecTransClient.dir/src/client/client.cpp.o:/home/ozeliurs/SecTrans/src/client/../lib/ultimateServer.h:8: first defined here
+[build] /usr/bin/ld: CMakeFiles/SecTransClient.dir/src/client/SecTransClient.cpp.o: in function `stop()':
+[build] /home/ozeliurs/SecTrans/src/client/../lib/ultimateServer.h:13: multiple definition of `stop()'; CMakeFiles/SecTransClient.dir/src/client/client.cpp.o:/home/ozeliurs/SecTrans/src/client/../lib/ultimateServer.h:13: first defined here
+[build] /usr/bin/ld: CMakeFiles/SecTransClient.dir/src/client/SecTransClient.cpp.o: in function `receive(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&)':
+[build] /home/ozeliurs/SecTrans/src/client/../lib/ultimateServer.h:18: multiple definition of `receive(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&)'; CMakeFiles/SecTransClient.dir/src/client/client.cpp.o:/home/ozeliurs/SecTrans/src/client/../lib/ultimateServer.h:18: first defined here
+[build] /usr/bin/ld: CMakeFiles/SecTransClient.dir/src/client/client.cpp.o: in function `send(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, int)':
+[build] /home/ozeliurs/SecTrans/src/client/../lib/ultimateClient.h:28: undefined reference to `sndmsg(char*, int)'
+[build] /usr/bin/ld: CMakeFiles/SecTransClient.dir/src/client/client.cpp.o: in function `start(int)':
+[build] /home/ozeliurs/SecTrans/src/client/../lib/ultimateServer.h:10: undefined reference to `startserver(int)'
+[build] /usr/bin/ld: CMakeFiles/SecTransClient.dir/src/client/client.cpp.o: in function `stop()':
+[build] /home/ozeliurs/SecTrans/src/client/../lib/ultimateServer.h:15: undefined reference to `stopserver()'
+[build] /usr/bin/ld: CMakeFiles/SecTransClient.dir/src/client/client.cpp.o: in function `receive(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&)':
+[build] /home/ozeliurs/SecTrans/src/client/../lib/ultimateServer.h:26: undefined reference to `getmsg(char*)'
+[build] /usr/bin/ld: CMakeFiles/SecTransClient.dir/src/client/SecTransClient.cpp.o: in function `send(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, int)':
+[build] /home/ozeliurs/SecTrans/src/client/../lib/ultimateClient.h:28: undefined reference to `sndmsg(char*, int)'
+[build] /usr/bin/ld: CMakeFiles/SecTransClient.dir/src/client/SecTransClient.cpp.o: in function `start(int)':
+[build] /home/ozeliurs/SecTrans/src/client/../lib/ultimateServer.h:10: undefined reference to `startserver(int)'
+[build] /usr/bin/ld: CMakeFiles/SecTransClient.dir/src/client/SecTransClient.cpp.o: in function `stop()':
+[build] /home/ozeliurs/SecTrans/src/client/../lib/ultimateServer.h:15: undefined reference to `stopserver()'
+[build] /usr/bin/ld: CMakeFiles/SecTransClient.dir/src/client/SecTransClient.cpp.o: in function `receive(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&)':
+[build] /home/ozeliurs/SecTrans/src/client/../lib/ultimateServer.h:26: undefined reference to `getmsg(char*)'
 [build] collect2: error: ld returned 1 exit status
-[build] gmake[3]: *** [CMakeFiles/SecTransServer.dir/build.make:122: SecTransServer] Error 1
-[build] gmake[2]: *** [CMakeFiles/Makefile2:124: CMakeFiles/SecTransServer.dir/all] Error 2
-[build] gmake[1]: *** [CMakeFiles/Makefile2:131: CMakeFiles/SecTransServer.dir/rule] Error 2
-[build] gmake: *** [Makefile:150: SecTransServer] Error 2
-[proc] The command: /usr/bin/cmake --build /home/ozeliurs/SecTrans/build --config Debug --target SecTransServer -j 4 -- exited with code: 2
-[driver] Build completed: 00:00:02.649
+[build] gmake[3]: *** [CMakeFiles/SecTransClient.dir/build.make:122: SecTransClient] Error 1
+[build] gmake[2]: *** [CMakeFiles/Makefile2:97: CMakeFiles/SecTransClient.dir/all] Error 2
+[build] gmake[1]: *** [CMakeFiles/Makefile2:104: CMakeFiles/SecTransClient.dir/rule] Error 2
+[build] gmake: *** [Makefile:137: SecTransClient] Error 2
+[proc] The command: /usr/bin/cmake --build /home/ozeliurs/SecTrans/build --config Debug --target SecTransClient -j 4 -- exited with code: 2
+[driver] Build completed: 00:00:01.285
 [build] Build finished with exit code 2
 */
 
